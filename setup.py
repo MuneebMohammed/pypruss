9bf49be96b8ec8c6de42388e3ec8667f82bb3a37
Muneeb Mohammed <mohammedabdulmuneeb98@gmail.com>
2018-05-25 04:33:29 +0300

    Add setup.py for package install

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+        name    = 'PyPRUSS',
+        url     = 'https://github.com/MuneebMohammed/pypruss/pypruss',
+        author  = 'Mohammed Muneeb',
+        author_email = 'mohammedabdulmuneeb98@gmail.com',
+        packages = ['pypruss'],
+        install_requires = [],
+        version = '0.1',
+        license = 'GPL',
+        description = 'A python package to interact with the Beaglebone PRUs',
+    )
+
